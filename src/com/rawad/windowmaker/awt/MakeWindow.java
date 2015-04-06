08d7122a21cbde5d0aa910fe6de3e947e067d086
Rawad Aboudlal <Rawadaboudlal@yahoo.com>
2015-04-06 16:44:18 -0400

    Added more in SWT.

--- src/com/rawad/windowmaker/awt/MakeWindow.java
+++ src/com/rawad/windowmaker/awt/MakeWindow.java
@@ -1,14 +1,24 @@
 package com.rawad.windowmaker.awt;
 
+import org.eclipse.core.databinding.DataBindingContext;
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.widgets.TabFolder;
+import org.eclipse.swt.widgets.TabItem;
 import org.eclipse.wb.swt.SWTResourceManager;
 
 public class MakeWindow {
 
+	private DataBindingContext m_bindingContext;
+
 	protected Shell shlFrameTitle;
+	private TabFolder tabFolder;
 
 	/**
 	 * Launch the application.
@@ -16,12 +26,17 @@ public class MakeWindow {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		try {
-			MakeWindow window = new MakeWindow();
-			window.open();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		Display display = Display.getDefault();
+		Realm.runWithDefault(SWTObservables.getRealm(display), new Runnable() {
+			public void run() {
+				try {
+					MakeWindow window = new MakeWindow();
+					window.open();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
 	/**
@@ -47,10 +62,28 @@ public class MakeWindow {
 		shlFrameTitle
 				.setImage(SWTResourceManager
 						.getImage("C:\\Users\\Rawad\\Documents\\Programming Tools\\Window Maker\\res\\test.png"));
-		shlFrameTitle.setMinimumSize(new Point(500, 500));
+		shlFrameTitle.setMinimumSize(new Point(0, 0));
 		shlFrameTitle.setSize(500, 500);
 		shlFrameTitle.setText("Frame Title");
+		shlFrameTitle.setLayout(new FillLayout(SWT.HORIZONTAL));
+
+		Composite composite = new Composite(shlFrameTitle, SWT.NONE);
+		composite.setLayout(new FillLayout(SWT.HORIZONTAL));
+
+		tabFolder = new TabFolder(composite, SWT.BORDER);
+
+		TabItem tbtmNewItem = new TabItem(tabFolder, SWT.NONE);
+		tbtmNewItem.setText("Text Editor");
+
+		TabItem tbtmNewItem_1 = new TabItem(tabFolder, SWT.NONE);
+		tbtmNewItem_1.setText("Image Editor");
+		m_bindingContext = initDataBindings();
 
 	}
 
+	protected DataBindingContext initDataBindings() {
+		DataBindingContext bindingContext = new DataBindingContext();
+		//
+		return bindingContext;
+	}
 }
